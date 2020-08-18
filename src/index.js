aa090a46f8cc803990a6af8b1e75bd432002eea1
Petros Kalogiannakis <github@petroskal.com>
2020-08-18 17:38:25 +0300

    Update index.js with layout components

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Container } from './components/Container';
+import { Main } from './components/Main';
+import { Layout } from './components/Layout';
 import { Albums } from './app/Albums';
 import { Tracks } from './app/Tracks';
 
@@ -13,10 +15,14 @@ const App = () => {
   return (
     <Container>
       <Router>
-        <Switch>
-          <Route path="/album/:id" component={Tracks} />
-          <Route path="/" component={Albums} />
-        </Switch>
+        <Main>
+          <Layout>
+            <Switch>
+              <Route path="/album/:id" component={Tracks} />
+              <Route path="/" component={Albums} />
+            </Switch>
+          </Layout>
+        </Main>
       </Router>
     </Container>
   );
