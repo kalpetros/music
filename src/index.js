2cb800a423ad0a88f6eedeb88c036edee21c8f83
Petros Kalogiannakis <github@petroskal.com>
2020-08-18 16:20:01 +0300

    Add entry js file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'normalize.css';
+import './styles/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+  return <div></div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
