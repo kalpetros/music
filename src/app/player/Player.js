9c5bdfc96887e1c19e8455e903bb3c56e288bc7c
Petros Kalogiannakis <github@petroskal.com>
2020-08-18 17:47:31 +0300

    Add player component

---
+++ src/app/player/Player.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/app/player/player.css';
+
+import React from 'react';
+
+export const Player = () => {
+
+  return (
+    <div className={styles.player}>
+    </div>
+  );
+};
