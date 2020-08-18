a621096aacd0070c2d93fc08303fe8c941c0e2f8
Petros Kalogiannakis <github@petroskal.com>
2020-08-18 18:16:01 +0300

    Use context in albums component

--- src/app/Albums.js
+++ src/app/Albums.js
@@ -1,22 +1,31 @@
 import styles from '../styles/app/albums.css';
 
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { Album } from './Album';
+import { AlbumContext } from '../store/AlbumContext';
 
 export const Albums = () => {
-  const items = Array(5)
-    .fill(1)
-    .map((album, index) => {
-      return (
-        <Album
-          key={`album-${index}`}
-          id={1}
-          title='Title'
-          cover=''
-          releaseYear='2020'
-        />
-      );
-    });
+  const { albums, setSelectedAlbum } = useContext(AlbumContext);
+
+  useEffect(() => {
+    setSelectedAlbum({});
+  }, [setSelectedAlbum]);
+
+  const items = albums.map((album, index) => {
+    const albumId = album.id;
+    const title = album.title;
+    const cover = album.cover.url;
+    const releaseYear = new Date(album.release_date).getFullYear();
+    return (
+      <Album
+        key={`album-${index}`}
+        id={albumId}
+        title={title}
+        cover={cover}
+        releaseYear={releaseYear}
+      />
+    );
+  });
   return <div className={styles.albums}>{items}</div>;
 };
